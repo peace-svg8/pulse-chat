9b1e3a3c19716f4673ede90745956243486db5db
amehin peace <amehinpeace.dev>
2026-05-20 02:17:42 -0700

    Support VITE_SERVER_URL environment variable

--- client/src/socket.ts
+++ client/src/socket.ts
@@ -4,7 +4,7 @@
 
 import { io, Socket } from 'socket.io-client';
 
-const SERVER_URL = 'http://localhost:3001';
+const SERVER_URL = import.meta.env.VITE_SERVER_URL || 'http://localhost:3001';
 
 export const socket: Socket = io(SERVER_URL, {
   autoConnect: false,
